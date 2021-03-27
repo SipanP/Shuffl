dcb80b8ad6ad6fa42f9a239501b38777ecad210e
Sipan Petrosyan <sipanpetrosyan0@gmail.com>
2021-03-27 17:11:32 +0000

    Initial Commit

---
+++ app/src/main/java/com/shufflteam/shuffl/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shufflteam.shuffl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
