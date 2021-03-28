82ddc16b639456d6422e327a584947fbbf80ad89
Vincent <vl620@ic.ac.uk>
2021-03-28 11:19:38 +0100

    Generates Random Circles

--- app/src/main/java/com/shufflteam/shuffl/User.java
+++ app/src/main/java/com/shufflteam/shuffl/User.java
@@ -1,18 +1,35 @@
 package com.shufflteam.shuffl;
 
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
 public class User {
 
     private final Point position;
     private final String username;
+    private final Button ball;
 
-
-    public User(String username) {
-        this(0, 0, username);
+    public User(String username, Button ball) {
+        this(0, 0, username, ball);
     }
 
-    public User(float x, float y, String username) {
+    public User(float x, float y, String username, Button ball) {
         position = new Point(x, y);
         this.username = username;
+        this.ball = ball;
+        ball.setText(getInitial());
+        float scale = ball.getContext().getResources().getDisplayMetrics().density;
+        int pixels = (int) (50 * scale + 0.5f);
+        RelativeLayout.LayoutParams rl = new RelativeLayout.LayoutParams(pixels, pixels);
+        rl.leftMargin = (int) x;
+        rl.topMargin = (int) y;
+        ball.setLayoutParams(rl);
+        ball.setBackgroundResource(R.drawable.round_button);
+        ball.setGravity(Gravity.CENTER);
+        ball.setTextColor(Color.WHITE);
+        ball.setTextSize(18);
     }
 
     public Point getPosition() {
@@ -20,6 +37,6 @@ public class User {
     }
 
     public String getInitial() {
-        return username.substring(0,1);
+        return username.substring(0,1).toUpperCase();
     }
 }
