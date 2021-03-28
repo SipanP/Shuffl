7eee990b8feb2c128a7b523395266984d179e4ac
Nash, Miles <mn820@ic.ac.uk>
2021-03-28 03:22:00 +0100

    User and Point complexity reduced

--- app/src/main/java/com/shufflteam/shuffl/User.java
+++ app/src/main/java/com/shufflteam/shuffl/User.java
@@ -1,82 +1,21 @@
 package com.shufflteam.shuffl;
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RelativeLayout;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class User extends AppCompatActivity implements View.OnTouchListener {
+public class User {
 
     private final Point position;
     private final String username;
-    private float dX, dY;
-    private Button ball;
-    private View ballView;
 
-    public User(String username){
-        position = new Point(0,0);
+
+    public User(String username) {
+        this(0, 0, username);
+    }
+
+    public User(float x, float y, String username) {
+        position = new Point(x, y);
         this.username = username;
-
-        ballView = (View) findViewById(R.id.ballView);
-        ball = (Button) findViewById(R.id.user1);
-        ball.setOnTouchListener(this);
     }
 
     public Point getPosition() {
         return position;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_room);
-
-        System.out.println("Created");
-    }
-
-    @Override
-    public boolean onTouch(View view, MotionEvent event) {
-        System.out.println("Clicked");
-
-        switch (event.getAction()) {
-
-            case MotionEvent.ACTION_DOWN:
-
-                dX = view.getX() - event.getRawX();
-                dY = view.getY() - event.getRawY();
-                break;
-
-            case MotionEvent.ACTION_MOVE:
-
-                view.animate()
-                        .x(event.getRawX() + dX)
-                        .y(event.getRawY() + dY)
-                        .setDuration(0)
-                        .start();
-                position.setTo((event.getRawX() + dX),(event.getRawY() + dY));
-                updateVolume();
-                break;
-            default:
-                return false;
-        }
-        return true;
-    }
-
-    public void updateVolume(){
-        AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-        // Takes distance to speaker / total possible distance
-        double screenRatio = position.distanceTo(5);
-        // Sets volume to MAXVOLUME * the above ratio (NEED CONSTANTS)
-        // STREAM_MUSIC is dependant on the kind of audio used (If a call, use RING or something)
-        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, (int) Math.round(screenRatio * audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)),0);
     }
 }
